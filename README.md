caad26923319d1984e1626f8f3525ddfb4089c3a
CourtneyB <cmbatiste@hotmail.com>
2021-08-09 13:13:30 -0500

    Initial Commit

---
+++ README.md
@@ -0,0 +1,1 @@
+# Learn Terraform - Provision a GKE Autopilot
---
+++ versions.tf
@@ -0,0 +1,11 @@
+terraform {
+  required_providers {
+    google-beta = {
+      source  = "hashicorp/google-beta"
+      version = "3.75.0"
+    }
+  }
+
+  required_version = "~> 1.0.0"
+}
+
